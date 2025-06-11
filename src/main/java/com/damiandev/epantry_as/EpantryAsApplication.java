5f0d9e6803bcba14d52f815cb21a7908173abc0d
damiankalicki <damikalicki@gmail.com>
2025-06-11 18:59:41 +0200

    Init springboot initializr

---
+++ src/main/java/com/damiandev/epantry_as/EpantryAsApplication.java
@@ -0,0 +1,13 @@
+package com.damiandev.epantry_as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpantryAsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpantryAsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damiandev/epantry_as/EpantryAsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damiandev.epantry_as;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpantryAsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
